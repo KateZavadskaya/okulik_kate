e4592338cdb22aa34186dd20999af71c178b4984
Kate Zavadskaya <ekm.zavadskaya@gmail.com>
2025-05-22 13:33:00 +0300

    Chore(doc): add actions
    Add actions, main.py, for_example.py

---
+++ main.py
@@ -0,0 +1,10 @@
+""" 'main' function prints the string 'Hello world!' to the console."""
+
+
+def main():
+    """ 'main' function prints  the string 'Hello world!' to the console."""
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
